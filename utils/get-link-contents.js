a30fe5b999013a47f5a9b2e2b71a483610ff2905
Gabriel Lanzani <gabriel.lanzani@centricdigital.com>
2019-05-21 11:00:42 -0300

    get link content fixes

--- utils/get-link-contents.js
+++ utils/get-link-contents.js
@@ -1,26 +1,25 @@
-var q = require('q')
-var request = require('request')
+var q = require("q")
+var request = require("request")
 
 module.exports = function getLinkContents(linkUrl, options) {
-  var d = q.defer()
+ var d = q.defer()
 
-  // expect linked css content
-  if (!/\.css.*$/i.test(linkUrl)) {
-    d.resolve('')
-    return d.promise
-  }
+ request({ url: linkUrl, timeout: options.timeout, gzip: true }, function(
+   error,
+   response,
+   body
+ ) {
+   if (error || response.statusCode !== 200) {
+     d.reject(error)
+   }
 
-  request({ url: linkUrl, timeout: options.timeout, gzip: true }, function(
-    error,
-    response,
-    body
-  ) {
-    if (error || response.statusCode !== 200) {
-      d.reject(error)
-    }
+   // expect linked css content
+   if (!response.headers["content-type"].includes("text/css")) {
+     d.resolve('');
+   }
 
-    d.resolve(body)
-  })
+   d.resolve(body)
+ });
 
-  return d.promise
+ return d.promise
 }
